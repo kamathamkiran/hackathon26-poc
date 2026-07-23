9cbe16f8d6fb70fb1463c1764bf6c997d98136bc
CHBob <srivatsavantmuri@gmail.com>
2026-07-23 15:40:05 +0530

    upload pdf to gcs bucket

--- src/main/java/com/db/hackathon/controller/WorkflowController.java
+++ src/main/java/com/db/hackathon/controller/WorkflowController.java
@@ -1,17 +1,15 @@
 package com.db.hackathon.controller;
 
-import com.db.hackathon.dto.WorkflowResponse;
 import com.db.hackathon.service.WorkflowService;
+import com.db.hackathon.subscribe.PdfUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.nio.file.Path;
 
 @RestController
 @RequestMapping("/workflow")
@@ -19,6 +17,7 @@ import java.nio.file.Path;
 public class WorkflowController {
 
     private final WorkflowService workflowService;
+    private final PdfUploadService pdfUploadService;
 
     @PostMapping(
             value = "/process",
@@ -31,4 +30,19 @@ public class WorkflowController {
         return;
     }
 
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadPdf(
+            @RequestParam("uuid") String uuid,
+            @RequestParam("username") String username,
+            @RequestParam("file") MultipartFile file) {
+
+        try {
+            String fileUrl = pdfUploadService.uploadPdf(uuid, username, file);
+            return ResponseEntity.ok("File uploaded successfully: " + fileUrl);
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest()
+                    .body("Upload failed: " + e.getMessage());
+        }
+    }
 }
